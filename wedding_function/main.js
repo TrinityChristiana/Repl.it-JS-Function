b47a742659b815f29c9a269a6a8e1dc060bbb5ae
TrinityTerry <trinitycterry@gmail.com>
2019-12-23 21:13:13 -0600

    first commit

---
+++ wedding_function/main.js
@@ -0,0 +1,11 @@
+// Define your function here that meets the requirements
+let announcement = (person1, person2) => {
+    let message = `Ladies and Gentlemen, please welcome the newlyweds ${person1} and ${person2}!!`;
+     window.alert(message);
+     return message;
+    
+  }
+  
+  // Invoke the function here and pass in two names as arguments
+  
+  announcement("gabrielle Terry", "Celeste Name");
